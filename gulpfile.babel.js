9ffd1f1c8d16ddea4071ca60d5e15e41f5cdbe1f
ricardovalquin <ricardovalquin@gmail.com>
2016-05-08 17:28:43 -0500

    initial commit

---
+++ gulpfile.babel.js
@@ -0,0 +1,76 @@
+// generated on 2016-05-08 using generator-ninthlink-email 1.0.9
+import gulp from 'gulp';
+import gulpLoadPlugins from 'gulp-load-plugins';
+import browserSync from 'browser-sync';
+import del from 'del';
+import inlineCss from 'gulp-inline-css';
+
+const $ = gulpLoadPlugins();
+const reload = browserSync.reload;
+
+
+gulp.task('html', () => {
+  return gulp.src('app/*.html')
+    .pipe($.useref({searchPath: ['app', '.']}))
+    .pipe(inlineCss())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('images', () => {
+  return gulp.src('app/images/**/*')
+    .pipe($.cache($.imagemin({
+      progressive: true,
+      interlaced: true,
+      // don't remove IDs from SVGs, they are often used
+      // as hooks for embedding and styling
+      svgoPlugins: [{cleanupIDs: false}]
+    })))
+    .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('extras', () => {
+  return gulp.src([
+    'app/*.*',
+    '!app/*.html'
+  ], {
+    dot: true
+  }).pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', del.bind(null, ['dist']));
+
+
+gulp.task('serve', () => {
+  browserSync({
+    notify: false,
+    port: 9000,
+    server: {
+      baseDir: ['app']
+    }
+  });
+
+  gulp.watch([
+    'app/*.html',
+    'app/images/**/*'
+  ]).on('change', reload);
+
+});
+
+gulp.task('serve:dist', () => {
+  browserSync({
+    notify: false,
+    port: 9000,
+    server: {
+      baseDir: ['dist']
+    }
+  });
+});
+
+
+gulp.task('build', ['html', 'images', 'extras'], () => {
+  return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
+});
+
+gulp.task('default', ['clean'], () => {
+  gulp.start('build');
+});
